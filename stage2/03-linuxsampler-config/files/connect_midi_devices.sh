36bda44073f6e76f7b9decac0836524c114aee83
Juan Pumarino <juan@scribd.com>
2017-06-18 11:02:33 -0700

    Connect everything using Midi Through port

--- stage2/03-linuxsampler-config/files/connect_midi_devices.sh
+++ stage2/03-linuxsampler-config/files/connect_midi_devices.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-/usr/bin/aconnect 'Arturia MiniLab mkII' 'Elektron Digitakt'
-/usr/bin/aconnect 'Arturia MiniLab mkII' DeepMind12
 /usr/bin/aconnect 'Arturia MiniLab mkII' 'Midi Through'
-/usr/bin/aconnect 'Arturia MiniLab mkII' mio
-/usr/bin/aconnect 'Elektron Digitakt' DeepMind12
-/usr/bin/aconnect 'Elektron Digitakt' mio
-/usr/bin/aconnect DeepMind12 'Elektron Digitakt'
+/usr/bin/aconnect 'Midi Through' 'Arturia MiniLab mkII'
+/usr/bin/aconnect 'Elektron Digitakt' 'Midi Through'
+/usr/bin/aconnect 'Midi Through' 'Elektron Digitakt'
 /usr/bin/aconnect DeepMind12 'Midi Through'
-/usr/bin/aconnect DeepMind12 mio
-/usr/bin/aconnect mio 'Elektron Digitakt'
+/usr/bin/aconnect 'Midi Through' DeepMind12
+/usr/bin/aconnect mio 'Midi Through'
+/usr/bin/aconnect 'Midi Through' mio
